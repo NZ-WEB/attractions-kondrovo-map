0429989c84ff6ede76c00ea9792c6097d0efc931
NZ-WEB <nzweb@vk.com>
2022-06-16 14:12:02 +0300

    feat(base-layout): add data

---
+++ src/components/TheDescription.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="description">
+    <h5 class="q-my-md">{{ title }}</h5>
+
+    <p v-for="textItem of text" :key="textItem">{{textItem}}</p>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  title: {
+    type: String
+  },
+  text: {
+    type: Array,
+  }
+})
+
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -4,24 +4,53 @@
       <div class="map">
         <q-img class="map__bg" src="~assets/kondr.png"/>
         <div class="map__pin chram">
-          <q-btn size="sm" round color="primary" icon="star"></q-btn>
+          <q-btn @click="handleChange('charge')" :size="getSize('charge')" round :color="getColor('charge')" icon="star"></q-btn>
         </div>
+
+        <div class="map__pin school">
+          <q-btn  @click="handleChange('school')" :size="getSize('school')" round :color="getColor('school')" icon="star"></q-btn>
+        </div>
+
+
       </div>
 
-      <div class="description">
-        <h5 class="q-my-md">Кондровская  средняя общеобразовательная  школа №1</h5>
-        <p>Кондровская  средняя общеобразовательная  школа №1 была открыта в 1937 году. Из переполненной  школы  второй ступени  перевели в "новостройку" часть учеников</p>
+      <the-description :title="currentTitle" :text="currentTexts"/>
 
-        <p>В годы Великой Отечественной войны с 1941 г. по 1943 г. в здании школы располагался госпиталь, учеба началась в стенах "новостройки"  лишь с осени 1943 года. Первый выпуск учащихся пришелся на второе послевоенное лето.</p>
-
-        <p>На территории школы открыты два памятника: один напротив входа в честь Учителей, которые ушли на фронт и не вернулись. 23 апреля 2015 г. на этом памятнике была открыта Мемориальная доска участникам и ветеранам  Великой Отечественной войны, работавшим в школе.</p>
-      </div>
     </div>
   </q-page>
 </template>
 
 <script setup>
 
+import TheDescription from "components/TheDescription";
+import { ref } from "vue";
+import {data} from "src/data/data";
+
+const active = ref('school')
+
+const currentTitle = ref(data.school.title);
+const currentTexts = ref(data.school.texts);
+
+const handleChange = (item) => {
+  currentTitle.value = data[item].title;
+  currentTexts.value = data[item].texts;
+
+  active.value = item
+}
+
+const getColor = (item) => {
+  if (active.value === item) {
+    return 'red'
+  }
+  return 'primary'
+}
+
+const getSize = (item) => {
+  if (active.value === item) {
+    return 'md'
+  }
+  return 'sm'
+}
 </script>
 
 <style lang="scss">
@@ -49,6 +78,11 @@
 .chram {
   top: 41%;
   left: 52%;
+}
+
+.school {
+  top: 20%;
+  left: 53.5%;
 }
 
 
